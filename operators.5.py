4bbc5272752aa25872218137d572f1535c30f2ba
jeebanjagdev5 <jeebanjagdev5@gmail.com>
2022-08-27 21:01:33 +0530

    first commit

---
+++ operators.5.py
@@ -0,0 +1,13 @@
+a,b=[float(i) for i in input("enter two numbers:").split()]
+
+#for greater number
+if a>b:
+    print(a,'is greater')
+else:
+    print(b,'is greater')
+
+#for smaller number
+if a<b:
+    print(a,'is smaller')
+else:
+    print(b,'is smaller')
